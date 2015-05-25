a55e24ca7621956b5e3d89ce2ebeff150fe1a904
movelikeriver <noemail@local.com>
2015-05-24 21:15:20 -0700

    basic SimpleApp

--- spark_fun/src/main/scala/SimpleApp.scala
+++ spark_fun/src/main/scala/SimpleApp.scala
@@ -5,7 +5,7 @@ import org.apache.spark.SparkConf
 
 object SimpleApp {
   def main(args: Array[String]) {
-    val logFile = "/Users/lenjoy/mysrc/spark-1.3.1/README.md" // Should be some file on your system
+    val logFile = "/tmp/README.md" // Should be some file on your system
     val conf = new SparkConf().setAppName("Simple Application")
     val sc = new SparkContext(conf)
     val logData = sc.textFile(logFile, 2).cache()
