3ab1438dc3860a9d51cb87ff2c4055952eee72be
movelikeriver <noemail@local.com>
2015-05-24 21:51:18 -0700

    SimpleApp.scala

--- spark_fun/src/main/scala/SimpleApp.scala
+++ spark_fun/src/main/scala/SimpleApp.scala
@@ -5,12 +5,19 @@ import org.apache.spark.SparkConf
 
 object SimpleApp {
   def main(args: Array[String]) {
-    val logFile = "/tmp/README.md" // Should be some file on your system
+    val logFile = "/tmp/sample.log" // Should be some file on your system
     val conf = new SparkConf().setAppName("Simple Application")
     val sc = new SparkContext(conf)
     val logData = sc.textFile(logFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
-    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+
+    val lines = sc.textFile(logFile);
+    val lineNum = lines.map(s => 1)
+    val totalLines = lineNum.reduce((a, b) => a + b)
+    val lineLengths = lines.map(s => s.length)
+    val totalLengths = lineLengths.reduce((a, b) => a + b)
+
+    println("Lines with a: %s, Lines with b: %s, Total length: %s lines: %s".format(numAs, numBs, totalLines, totalLengths))
   }
 }
